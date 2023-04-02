b5a540094cc5311c6b43f0c629e4fd1daf447949
Jose Vargas <129638981+josepavaa118@users.noreply.github.com>
2023-04-01 21:54:13 -0600

    Core Commit
    Basic commit with core project, basic tests and fixture file usage

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,28 @@
+import { should } from "chai"
+import * as appSignIn from "../selectors/signInPage.sel"
+import * as appHome from "../selectors/homePage.sel"
+
+describe('template spec', () => {
+  let uiTexts: any
+  before(() => {
+    cy.fixture("uiTexts").then((data) => {
+      uiTexts = data
+      return uiTexts
+    });
+  });
+  beforeEach("Open App", () => {
+    // Command to visit home page each time a test starts
+    cy.openHomePage()
+  });
+  
+  it('Should display Home Page', () => {
+    appHome.printStuff()
+    appHome.checkIfHomePage(appHome.homePageHeader)
+  })
+  
+  it('Should redirect to Sign In Page', () => {
+    cy.get(appHome.signInLink ).click()
+    cy.get(appSignIn.pageHeader).should('be.visible').should('have.text',uiTexts.signInPageHeader)
+
+  })
+})

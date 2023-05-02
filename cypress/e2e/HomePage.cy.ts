6cea446115e49465ad3e8ea4911dd44ab50e4a7f
Jose Vargas <129638981+josepavaa118@users.noreply.github.com>
2023-05-02 17:00:02 -0600

    Overall Refactor
    This PR cleans several unused spaces, unused imports, unnecessary comments,  and the Config file. Also adds POM Support for Tags spec

--- cypress/e2e/HomePage.cy.ts
+++ cypress/e2e/HomePage.cy.ts
@@ -1,28 +1,24 @@
-import { should } from "chai"
 import * as appSignIn from "../selectors/signInPage.sel"
 import * as appHome from "../selectors/homePage.sel"
 
-describe('template spec', () => {
+describe('Home Page Test Cases', () => {
   let uiTexts: any
   before(() => {
-    cy.fixture("uiTexts").then((data) => {
+    cy.fixture("uiTexts").then((data) => {//From here UITexts are stored for further usage
       uiTexts = data
       return uiTexts
     })
   })
-  beforeEach("Open App", () => {
-    // Command to visit home page each time a test starts
+  beforeEach("Open App", () => {// Command to visit home page each time a test starts
     cy.openHomePage()
-  });
+  })
   
   it('Should display Home Page', () => {
-    //appHome.checkIfHomePage(appHome.homePageHeader)
     cy.get(appHome.homePageHeader).should('be.visible').should('have.text',uiTexts.homePageHeader)
   })
   
   it('Should redirect to Sign In Page', () => {
     cy.get(appHome.signInLink ).click()
     cy.get(appSignIn.pageHeader).should('be.visible').should('have.text',uiTexts.signInPageHeader)
-
   })
 })
